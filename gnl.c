0d38ac09e39f93daf5989c4a9db77528bfb4a1e0
Akuma <86250671+rep-aku@users.noreply.github.com>
2022-11-15 23:54:18 +0100

    Get-Next-Line-One-Line

---
+++ gnl.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 19:18:20 by hahadiou          #+#    #+#             */
+/*   Updated: 2022/10/18 19:26:13 by hahadiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <unistd.h>
+
+char *gnl(int fd){char ret_line[100];char buffer;int i=0,rd=read(fd,&buffer,1);while(rd){ret_line[i++]=buffer;if(buffer=='\n')break;else rd=read(fd,&buffer,1);}char *s;int len=strlen(ret_line)+1;s=malloc(len);while(len--){s[len]=ret_line[len];}return(s);}
